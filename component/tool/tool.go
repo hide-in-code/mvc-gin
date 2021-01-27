df99210369d45d5f67802bcbfeb4712b1f72c4a6
hejinlong <hejinlong@testin.cn>
2021-01-27 13:30:59 +0800

    tool

--- component/tool/tool.go
+++ component/tool/tool.go
@@ -2,9 +2,23 @@ package tool
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
-//便捷打印
+//便捷打印, 类似php里面的var_dump
 func Dump(expression ...interface{}) {
 	fmt.Println(fmt.Sprintf("%#v", expression))
+}
+
+// RandStringRunes 返回随机字符串
+func RandStringRunes(n int) string {
+	var letterRunes = []rune("1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#$%&*=ßßßß")
+
+	rand.Seed(time.Now().UnixNano())
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
